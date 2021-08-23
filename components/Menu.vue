4de15c0548805983a6e3b92c2e0aebf08c53b924
yanhuaguo <yanhuaguo@zhangyue.com>
2021-08-23 18:57:17 +0800

    init project pages

---
+++ components/Menu.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu-item index="1">首页</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">类型支持</template>
+      <el-menu-item index="2-1">选项1</el-menu-item>
+      <el-menu-item index="2-2">选项2</el-menu-item>
+      <el-menu-item index="2-3">选项3</el-menu-item>
+    </el-submenu>
+    <el-submenu index="3">
+      <template slot="title">客户端支持</template>
+      <el-menu-item index="3-1">选项1</el-menu-item>
+      <el-menu-item index="3-2">选项2</el-menu-item>
+      <el-menu-item index="3-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: '1'
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      // console.log(key, keyPath)
+    }
+  }
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-container>
+    <el-header>
+      <Menu/>
+    </el-header>
+    <el-main>
+      <div>
+        Redis UI is a very functional handy database GUI and works in your pocket on the responsive web or as a desktop
+        app v2021.10.106
+      </div>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import Menu from '~/components/Menu'
+
+export default {
+  name: 'About',
+  components: {
+    Menu
+  },
+  data() {
+    return {}
+  },
+  watch: {},
+  beforeDestroy() {
+  },
+  beforeMount() {
+  },
+  mounted() {
+
+  },
+  methods: {},
+  head() {
+    return {
+      title: '介绍'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
